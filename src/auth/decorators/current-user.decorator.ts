1c3ac51187b74d10d67c28ac77dc2d3a4a0d607c
KhaliRasbajus <kevinsalamanca150920@gmail.com>
2024-09-07 12:28:32 -0500

    Cambios

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,39 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  ForbiddenException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Roles } from '../enums/roles-user.enum';
+import { CurrentUser } from '../interfaces';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUsers = createParamDecorator(
+  (roles: Roles[], context: ExecutionContext) => {
+    const ctxType = context.getType<string>();
+
+    let request: any;
+    if (ctxType === 'graphql') {
+      const gqlContext = GqlExecutionContext.create(context);
+      request = gqlContext.getContext().req;
+    } else {
+      // Asume que el contexto es HTTP si no es GraphQL
+      request = context.switchToHttp().getRequest();
+    }
+
+    const user: CurrentUser = request.user;
+    if (!user) {
+      throw new InternalServerErrorException(
+        'User not found in request (RolesGuard called?)',
+      );
+    }
+
+    if (roles.length === 0) return user;
+    for (const role of user.roles) {
+      if (roles.includes(role as Roles)) return user;
+    }
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role [${roles.join(', ')}]`,
+    );
+  },
+);
--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -7,6 +7,9 @@ import { ClientProxy } from '@nestjs/microservices';
 import { CategoryProductsInput, FindByValueInput } from './dto/inputs/category';
 import { CategoryProducts } from './entities';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { CurrentUsers } from 'src/auth/decorators/current-user.decorator';
+import { CurrentUser } from 'src/auth/interfaces';
+import { Roles } from 'src/auth/enums/roles-user.enum';
 @UseGuards(AuthGuard)
 @Resolver(() => Product)
 export class ProductsResolver {
@@ -15,6 +18,7 @@ export class ProductsResolver {
   @Mutation(() => CategoryProducts, { name: 'createCategory' })
   createCategory(
     @Args('createProductInput') categoryProductsInput: CategoryProductsInput,
+    @CurrentUsers([Roles.ADMIN]) admin: CurrentUser,
   ) {
     return this.client.send('product.category.create', categoryProductsInput);
   }
@@ -32,13 +36,17 @@ export class ProductsResolver {
   }
 
   @Mutation(() => CategoryProducts, { name: 'deleteCategory' })
-  deleteCategory(@Args('name') name: string) {
+  deleteCategory(
+    @Args('name') name: string,
+    @CurrentUsers([Roles.ADMIN]) admin: CurrentUser,
+  ) {
     return this.client.send('product.category.delete', name);
   }
 
   @Mutation(() => Product, { name: 'createProduct' })
   createProduct(
     @Args('updateProductInput') createProductInput: CreateProdctsInput,
+    @CurrentUsers([Roles.ADMIN]) admin: CurrentUser,
   ) {
     return this.client.send('product.create', createProductInput);
   }
@@ -47,18 +55,25 @@ export class ProductsResolver {
     return this.client.send('product.findAll', {});
   }
   @Query(() => Product, { name: 'findOneProduct' })
-  findOneProduct(@Args('id', { type: () => Int }) id: number) {
+  findOneProduct(
+    @Args('id', { type: () => Int }) id: number,
+    @CurrentUsers() admin: CurrentUser,
+  ) {
     return this.client.send('product.findOne', id);
   }
 
   @Mutation(() => Product, { name: 'updateProduct' })
   updateProduct(
     @Args('updateProductInput') updateProductsInput: UpdateProductsInput,
+    @CurrentUsers([Roles.ADMIN]) admin: CurrentUser,
   ) {
     return this.client.send('product.update', updateProductsInput);
   }
   @Mutation(() => Product)
-  removeProduct(@Args('id', { type: () => Int }) id: number) {
+  removeProduct(
+    @Args('id', { type: () => Int }) id: number,
+    @CurrentUsers([Roles.ADMIN]) admin: CurrentUser,
+  ) {
     return this.client.send('product.delete', id);
   }
 }
