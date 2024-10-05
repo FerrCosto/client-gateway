b2fc2e9ab14447cee502f22e35e149970e1dc961
KhaliRasbajus <kevinsalamanca150920@gmail.com>
2024-10-04 21:17:42 -0500

    cambios

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,17 +10,24 @@ import {
   UseGuards,
   HttpException,
   HttpStatus,
+  Query,
+  ParseIntPipe,
+  Res,
 } from '@nestjs/common';
 
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { NATS_SERVICE } from '../config/service.config';
-import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CurrentUsers } from 'src/auth/decorators/current-user.decorator';
 
 import { CurrentUser } from 'src/auth/interfaces';
 import { Roles } from 'src/auth/enums/roles-user.enum';
+
+import { firstValueFrom } from 'rxjs';
+import { Response } from 'express';
+import { Readable } from 'stream';
 
 @UseGuards(AuthGuard)
 @Controller('orders')
@@ -40,9 +47,41 @@ export class OrdersController {
     return this.client.send('orders.findAll', {});
   }
 
+  @Get('invoice')
+  async findInvoiceOrder(
+    @Query('order', ParseIntPipe) orderId: number,
+    @Res() response: Response,
+  ) {
+    try {
+      // Obtener la orden pagada
+      const orderPaid = await firstValueFrom(
+        this.client.send('order.findOnePaid', orderId),
+      );
+
+      // Generar el PDF utilizando el servicio de factura
+      const invoiceOrder = await firstValueFrom(
+        this.client.send('factura.create', orderPaid),
+      );
+
+      // Configurar los encabezados de la respuesta
+      response.setHeader('Content-Type', 'application/pdf');
+
+      response.setHeader(
+        'Content-Disposition',
+        'inline; filename=factura.pdf', // Cambiar a 'inline' si deseas que se muestre en el navegador
+      );
+      const pdf = Buffer.from(invoiceOrder, 'base64');
+
+      // Enviar el buffer del PDF como respuesta
+      response.end(pdf);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.client.send('order.findOne', {});
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.client.send('order.findOne', +id);
   }
 
   @Patch(':id')
